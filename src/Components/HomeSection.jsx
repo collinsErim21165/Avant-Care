faf6916401013215e56817cb8c35e4523bf2dfad
Collins Erim <collinserim72@gmail.com>
2025-04-09 20:54:21 +0100

    Home page background

--- src/Components/HomeSection.jsx
+++ src/Components/HomeSection.jsx
@@ -3,26 +3,26 @@ import firstImg from '../images/hospital-home.jpg';
 
 const HomeSection = () => {
   return (
-    <div className="relative w-full h-screen overflow-hidden">
-    <div className="fixed inset-0 z-0">
-      <img
-        src={firstImg}
-        alt="background"
-        className="w-full h-full object-cover"
-      />
-    </div>
-  
-    {/* Overlay */}
-    <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>
-  
-    {/* Content */}
-    <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1 }}
-      className="relative z-20 flex items-center justify-center h-full px-6 md:px-12 lg:px-20 text-white text-center max-w-3xl mx-auto"
+    <div 
+      className="relative w-full h-screen flex md:bg-fixed  md:mt-32 mt-28 items-center justify-center px-6 md:px-12 lg:px-20 "
+      style={{
+        backgroundImage: `url(${firstImg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        // backgroundAttachment: "fixed",
+      }}
     >
-      <div>
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+
+      {/* Content */}
+      <motion.div 
+        initial={{ opacity: 0, y: 50 }} 
+        animate={{ opacity: 1, y: 0 }} 
+        transition={{ duration: 1 }} 
+        className="relative z-10 text-white text-center max-w-3xl"
+      >
         <h1 className="text-3xl md:text-5xl font-bold leading-tight">
           Your Health, Our Priority
         </h1>
@@ -32,9 +32,8 @@ const HomeSection = () => {
         <button className="mt-6 px-6 py-3 bg-[#37489E] hover:bg-[rgb(255,186,90)] transition duration-300 rounded-lg text-white font-semibold text-lg">
           Book an Appointment
         </button>
-      </div>
-    </motion.div>
-  </div>
+      </motion.div>
+    </div>
   );
 };
 
